64fd5ec2a8bbf278d5a0eafb89100833db5e3163
snaco <jm.anderson.cs@gmail.com>
2023-01-26 20:20:10 -0500

    relative paths in override

--- override.sh
+++ override.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo rm /usr/share/regolith/i3/config.d/*
-sudo cp -r /home/james/i3_system_config/config.d/* /usr/share/regolith/i3/config.d/
-sudo cp /home/james/i3_system_config/config /etc/regolith/i3/config
+sudo cp -r ./config.d/* /usr/share/regolith/i3/config.d/
+sudo cp ./config /etc/regolith/i3/config
